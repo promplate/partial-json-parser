e034cb5de1d5bb0257d9c2a1cd69004b39a80856
Muspi Merol <wjj123345567@icloud.com>
2024-05-11 22:27:29 +0800

    [perf] try not avoid falling back to the old parser

--- src/partial_json_parser/core/myelin.py
+++ src/partial_json_parser/core/myelin.py
@@ -1,9 +1,10 @@
 """Myelin acts as the highway among neurons, epitomizing the leapfrog methodology within this algorithm."""
 
 from re import compile
-from typing import Union
+from typing import List, Tuple, Union
 
-from .complete import fix
+from .complete import complete_str, fix
+from .exceptions import PartialJSON
 from .options import *
 
 finditer = compile(r'["\[\]{}]').finditer
@@ -11,6 +12,10 @@ finditer = compile(r'["\[\]{}]').finditer
 
 def scan(json_string: str):
     return [(match.start(), match.group()) for match in finditer(json_string)]
+
+
+def join_closing_tokens(stack: List[Tuple[int, str]]):
+    return "".join("}" if char == "{" else "]" for _, char in reversed(stack))
 
 
 def fix_fast(json_string: str, allow_partial: Union[Allow, int] = ALL):
@@ -45,15 +50,53 @@ def fix_fast(json_string: str, allow_partial: Union[Allow, int] = ALL):
     if not stack:
         return json_string, ""
 
+    # check if the opening tokens are allowed
+
+    allow = Allow(allow_partial)
+
+    if OBJ not in allow:
+        for index, [i, char] in enumerate(stack):
+            if char == "{":
+                if index == 0:
+                    raise PartialJSON("Partial object is not allowed")
+                return json_string[:i], join_closing_tokens(stack[:index])
+    if ARR not in allow:
+        for index, [i, char] in enumerate(stack):
+            if char == "[":
+                if index == 0:
+                    raise PartialJSON("Partial array is not allowed")
+                return json_string[:i], join_closing_tokens(stack[:index])
+
+    if json_string[-1] in "]}" and not in_string:  # the last token is a closing token
+        return json_string[: i + 1], "".join("}" if char == "{" else "]" for _, char in reversed(stack))
+
+    if char == '"':
+        if in_string:
+            if STR not in allow:
+                i, char = tokens[-2]
+                if char != '"' or stack[-2][1] != "{":
+                    return json_string[: i + 1], join_closing_tokens(stack)
+            elif stack[-1][1] != "{":
+                index, tail = complete_str(json_string[i:], allow)  # type: ignore
+                return json_string[: i + index], tail + join_closing_tokens(stack)  # type: ignore
+
+        if stack[-1][1] == "[":
+            rest = json_string[i + 1 :].rstrip("-").strip().lstrip(",")  # maybe `, -`
+            if rest:
+                head, tail = fix("[" + rest, allow)
+                assert head[0] == "[" and tail[-1] == "]"
+                return json_string[: i + 1] + "," + head[1:], tail + join_closing_tokens(stack[:-1])
+            return json_string[: i + 1], join_closing_tokens(stack)
+
+    # worst case when the last container need to be parsed by the fallback parser
+
     index, char = stack.pop()
 
     last_entity = json_string[index:]
 
-    stack.reverse()
-
     if not last_entity:
-        return json_string[:index], "".join("}" if char == "{" else "]" for _, char in stack)
+        return json_string[:index], join_closing_tokens(stack)
 
     head, tail = fix(last_entity, allow_partial)
 
-    return json_string[:index] + head, tail + "".join("}" if char == "{" else "]" for _, char in stack)
+    return json_string[:index] + head, tail + join_closing_tokens(stack)
